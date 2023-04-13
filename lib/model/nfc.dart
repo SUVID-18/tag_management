d5ae8f06c9b89df623187da743880e5ae02b6a3a
sun30812 <sun30812@naver.com>
2023-04-13 15:23:42 +0900

    #9 NFC 객체 모델링 완료

---
+++ lib/model/nfc.dart
@@ -0,0 +1,40 @@
+class NfcObject {
+  /// NFC 태그의 UUID(자동 생성됨)
+  String uuid = 'UUID';
+
+  /// 강의실 이름(IT 404호)
+  String lectureRoom;
+
+  /// 강의실 태그 정보를 업로드 하기 위한 생성자
+  ///
+  /// 강의실 태그에 관한 정보를 담고있는 객체의 생성자이다. [lectureRoom]에 강의실 위치(IT 404호)를
+  /// 기입하면 된다.
+  NfcObject({this.uuid = '', required this.lectureRoom});
+
+  /// [json]에서 객체를 역직렬화 하는 경우(NFC 객체로 가져오기) 사용되는 `factory` 생성자
+  ///
+  /// `Firestore`에서 받은 데이터를 [NfcObject]객체로 반환하는 메서드로 [json]에
+  /// `Firestore`에서 받은 데이터를 넣으면 된다.
+  ///
+  /// ```dart
+  /// final lectureRoomRef = db.collection('lectureRooms').doc('test');
+  /// lectureRoomRef.get().then(
+  ///   (DocumentSnapshot doc) {
+  ///     final student = NfcObject.fromJson(jsonDecode(doc.data()));
+  ///    },
+  ///    onError: (e) => print('Error Detected: $e'),
+  ///   );
+  /// ```
+  factory NfcObject.fromJson(Map<String, dynamic> json) =>
+      NfcObject(uuid: json['uuid'], lectureRoom: json['lectureRoom']);
+
+  /// 객체를 `JSON`으로 직렬화 하는 메서드
+  ///
+  /// 객체를 `Firestore`에게 쉽게 올릴 수 있도록 직렬화를 수행한다.
+  /// 이 메서드는 별도로 호출될 필요 없이 `jsonEncode()`메서드에 사용된다.
+  ///
+  /// ```dart
+  /// String json = jsonEncode(nfc_object);
+  /// ```
+  Map<String, dynamic> toJson() => {'uuid': uuid, 'lectureRoom': lectureRoom};
+}
