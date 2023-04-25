a7f4b5eac261780cb28336a782b42cb530ba42a1
Planybirth <splashslayer@gmail.com>
2023-04-25 22:53:47 +0900

    팩터링 수정.

--- lib/viewmodel/nfc_management.dart
+++ lib/viewmodel/nfc_management.dart
@@ -6,9 +6,8 @@ import 'package:nfc_manager/nfc_manager.dart';
 /// NFC 태그를 읽고 쓰는 기능을 담은 뷰모델
 class NfcManagementViewModel {
 
-}
   /// NFC 태그 스티커에 기록된 dynamicLink를 수정하는 메서드. ( firebase 수정은 추후 작업 )
-  void tagWrite(String? uuid, String lectureRoom){
+  void tagWrite(String? uuid, String lectureRoom) {
     // 정보를 입력할 NFC 객체 생성
     NfcObject nfcObject = NfcObject(uuid: uuid, lectureRoom: lectureRoom);
 
@@ -16,7 +15,7 @@ class NfcManagementViewModel {
     Map<String, dynamic> json = nfcObject.toJson();
 
     // 태그에는 firebase dynamic link 하나만 들어간다.
-    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async{
+    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
       var ndef = Ndef.from(tag);
 
       final dynamicLinkParams = DynamicLinkParameters(
@@ -27,17 +26,19 @@ class NfcManagementViewModel {
         ),
       );
 
-      final dynamicLink = await FirebaseDynamicLinks.instance.buildLink(dynamicLinkParams);
+      final dynamicLink = await FirebaseDynamicLinks.instance.buildLink(
+          dynamicLinkParams);
 
       NdefMessage message = NdefMessage([NdefRecord.createUri(dynamicLink)]);
 
-      try{
+      try {
         await ndef?.write(message);
         NfcManager.instance.stopSession();
-      } catch(e){
+      } catch (e) {
         NfcManager.instance.stopSession(errorMessage: e.toString());
       }
 
       // firestore 데이터베이스에 기록 부분은 생략.
     });
+  }
 }
