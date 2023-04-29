6ea56cb05beac221e6e12b5143705a3ffa0180df
Planybirth <splashslayer@gmail.com>
2023-04-29 17:09:00 +0900

    #15 ManagementViewModel 추가.

--- test/model_test.dart
+++ test/model_test.dart
@@ -2,18 +2,15 @@ import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tag_management/model/nfc.dart';
+import 'package:tag_management/viewmodel/management.dart';
 // import 'package:uuid/uuid.dart';
 
 void main() {
-  group('NFC 객체 테스트', () {
-    test('NFC 객체가 정상적으로 생성되는지 테스트', () {
-      final object = NfcObject(lectureRoom: 'IT 404호', uuid: 'not-found-404');
-      expect(object.lectureRoom, 'IT 404호');
-    });
-    test('NFC 객체의 직렬화 테스트', () {
-      final object = NfcObject(lectureRoom: 'IT 404호', uuid: 'not-found-404');
-      final jsonData = jsonEncode(object);
-      expect(NfcObject.fromJson(jsonDecode(jsonData)).uuid, 'not-found-404');
+  group('ManagementViewmodel 테스트', () {
+    test('GetNfcObjectList 테스트', () {
+      final viewmodel = ManagementViewModel();
+      List<NfcObject> list = viewmodel.GetNfcObjectList();
+      expect(list[0].lectureRoom, 'IT 304');
     });
   });
 }
