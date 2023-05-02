4d8ffab8d493cbecd99aaf4c94a3a6c033f5f886
Planybirth <splashslayer@gmail.com>
2023-05-02 12:23:49 +0900

    #15 설정 페이지 기능을 담당하는 뷰모델 정의 및 불필요한 패키지 참조 제거.

--- test/management_model_test.dart
+++ test/management_model_test.dart
@@ -1,13 +1,12 @@
-import 'dart:convert';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tag_management/model/nfc.dart';
 import 'package:tag_management/viewmodel/management.dart';
-// import 'package:uuid/uuid.dart';
+
 
 void main() {
   group('ManagementViewmodel 테스트', () {
     test('GetNfcObjectList 테스트', () {
+      // firebase에 연동하여 테스트하는게 아니라 아직은 원활함.
       final viewmodel = ManagementViewModel();
       List<NfcObject> list = viewmodel.GetNfcObjectList();
       expect(list[0].lectureRoom, 'IT 304');
